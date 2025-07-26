8d8932159e387669a17cc5a4a6c23531efa5bf07
bekzatkhanDev <bekzatkhan.zhumabekov@gmail.com>
2025-07-26 22:20:15 +0500

    Changed env and firebase-admin ts

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,11 +2,18 @@ import { initializeApp, cert, getApps } from 'firebase-admin/app';
 
 export const initFirebaseAdmin = () => {
   if (!getApps().length) {
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+    const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+
+    if (!privateKey || !clientEmail) {
+      throw new Error("Firebase credentials are not set");
+    }
+
     initializeApp({
       credential: cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+        clientEmail,
+        privateKey: privateKey.replace(/\\n/g, '\n'),
       })
     });
   }
